272cae3e8848829c6a7941d4afcda797ccf7325d
OmarMWarraich <earthling986@gmail.com>
2021-04-29 03:15:32 +0500

    tours

--- src/App.js
+++ src/App.js
@@ -1,18 +1,57 @@
-import React, { useState } from 'react';
-import Data from './components/Data';
-import List from './components/List';
+import React, { useState, useEffect } from 'react';
+import Loading from './components/Loading';
+import Tours from './components/Tours';
+
+const url = 'https://course-api.com/react-tours-project'
 
 function App() {
-  const [people, setPeople ] = useState(Data)
+  const [ loading, setLoading ] = useState(true);
+  const [ tours, setTours ] = useState([])
+
+  const removeTour = (id) => {
+    const newTours = tours.filter((tour) => tour.id !== id)
+    setTours(newTours);
+  }
+
+  const fetchTours = async () => {
+    setLoading(true)
+    try {
+      const response = await fetch(url)
+      const tours = await response.json()
+      setLoading(false)
+      setTours(tours)
+    } catch (error) {
+      setLoading(false)
+      console.log(error)
+    }
+  }
+  useEffect(() => {
+    fetchTours()
+  }, [])
+  if (loading) {
+    return (
+      <main>
+        <Loading />
+      </main>
+    )
+  }
+  if (tours.length === 0) {
+    return (
+      <main>
+        <div className='title'>
+          <h2> no tours left </h2>
+          <button className='btn' onClick={() => fetchTours()}>
+            refresh
+          </button>
+        </div>
+      </main>
+    )
+  }
   return (
     <main>
-      <section className="container">
-        <h3>{people.length} birthdays todays</h3>
-        <List people={people} />
-        <button onClick={() => setPeople([])}>clear all</button>
-      </section>
+      <Tours tours={tours} removeTour={removeTour} />
     </main>
   )
 }
 
-export default App;
+export default App
---
+++ src/components/Loading.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const Loading = () => {
+    return (
+        <div className="laoding">
+            <h1>loading...</h1>
+            
+        </div>
+    )
+}
+export default Loading;
