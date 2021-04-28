65fd52f680932a99bd589136dfe5719fa10823fa
OmarMWarraich <earthling986@gmail.com>
2021-04-29 02:32:19 +0500

    first commit

---
+++ src/components/Data.js
@@ -0,0 +1,47 @@
+// https://depositphotos.com/31708571/stock-photo-young-woman-beside-old-metal.html
+// https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSd9VwEW5DYn4Z9HCIEY_XpAsV1vtoJxOsHzQ&usqp=CAU
+// https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSkwuH86lC2iiWZ9dlNfe3wmkVP4vUkyUtTTg&usqp=CAU
+
+
+// https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8cGVyc29ufGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60
+// https://images.unsplash.com/photo-1573140247632-f8fd74997d5c?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MjB8fHBlcnNvbnxlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60
+// https://images.unsplash.com/photo-1596215143922-eedeaba0d91c?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MzB8fHBlcnNvbnxlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60
+// https://images.unsplash.com/photo-1593219191991-47517a4bb8d5?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Njd8fHBlcnNvbnxlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60
+// https://images.unsplash.com/photo-1544098575-553f8a92c545?ixid=MnwxMjA3fDB8MHxzZWFyY2h8NzN8fHBlcnNvbnxlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60
+// https://images.unsplash.com/photo-1590895340509-793cb98788c9?ixid=MnwxMjA3fDB8MHxzZWFyY2h8OTR8fHBlcnNvbnxlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60
+
+
+export default [
+    {
+        id: 1,
+        name: 'Celina Parker',
+        age: 21,
+        image: 'https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8cGVyc29ufGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60',
+    },
+    {
+        id: 2,
+        name: 'Catherine Adams',
+        age: 25,
+        image: 'https://images.unsplash.com/photo-1573140247632-f8fd74997d5c?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MjB8fHBlcnNvbnxlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60',
+    },
+    {
+        id: 3,
+        name: 'Elizabeth Shaun',
+        age: 27,
+        image: 'https://images.unsplash.com/photo-1596215143922-eedeaba0d91c?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MzB8fHBlcnNvbnxlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60',
+    },
+    {
+        id: 4,
+        name: 'Micky Jones',
+        age: 29,
+        image: 'https://images.unsplash.com/photo-1593219191991-47517a4bb8d5?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Njd8fHBlcnNvbnxlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60',
+    },
+    {
+        id: 5,
+        name: 'Jacob Andrew',
+        age: 31,
+        image: 'https://images.unsplash.com/photo-1544098575-553f8a92c545?ixid=MnwxMjA3fDB8MHxzZWFyY2h8NzN8fHBlcnNvbnxlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60',
+    },
+    
+    
+];
